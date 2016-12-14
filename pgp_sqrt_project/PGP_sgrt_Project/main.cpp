ac7fc7605d4f7c8c9b1f2407fd7405ec413ff019
yBoBy <mts9635@googlemail.com>
2016-12-14 17:47:53 +0100

    Hinzufuegen meiner Dateien

---
+++ pgp_sqrt_project/PGP_sgrt_Project/main.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "Functions.h"
+
+
+
+sqrt_obj userInput() {
+	sqrt_obj tmp;
+	printf("Bitte geben sie die Zahl ein deren Quadratwurzel berechnet werden soll:\n");
+	scanf("%lf", &tmp.zahl);
+	printf("Bitte geben sie die Genauigkeit ein, mit der die Quadratwurzel berechnet werden soll:\n");
+	scanf("%lf", &tmp.genauigkeit);
+	tmp.ergebnis = 0;
+	return tmp;
+}
+
+
+
+
+void heron(heron_obj *ho, sqrt_obj *so) {
+	ho-> laenge = (ho->laenge + ho->breite)/2;
+	ho->breite = so->zahl / ho->laenge;
+	double differenz = ho->laenge - ho->breite;
+	if (differenz >= 0) {
+		ho->differenz = differenz;
+	}
+	else {
+		ho->differenz = differenz * (-1);
+	}
+
+	so->ergebnis = ho->laenge;
+
+}
+
+
+int main() {
+	sqrt_obj so = userInput();
+	heron_obj ho;
+	ho.laenge = so.zahl;
+	ho.breite = 1;
+
+	
+	heron(&ho, &so);
+	//führt Heron durch bis gewünschte Genauigkeit erreicht ist
+	while (ho.differenz > so.genauigkeit) {
+		heron(&ho, &so);
+	}
+
+	printf("Quadratwurzel: %lf\n Genauigkeit: %lf\n", so.ergebnis, so.genauigkeit);
+	while (getchar() == '\n') {
+
+	}
+	
+
+	return 0;
+}
+
